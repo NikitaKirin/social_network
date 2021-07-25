bf1c17f733b159a3b04176d799b5ba7cac04ddc0
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-07-25 11:21:18 +0500

    Реализована возможность комментирования страниц пользователей.Мелкие правки.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,23 @@
         @csrf
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+            <input type="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1"
+                   aria-describedby="emailHelp" name="email">
+            @error('email')
+            <span class="invalid-feedback">
+                {{ $message }}
+            </span>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Пароль</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                   id="exampleInputPassword1" name="password">
+            @error('password')
+            <span class="invalid-feedback">
+                {{ $message }}
+            </span>
+            @enderror
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
